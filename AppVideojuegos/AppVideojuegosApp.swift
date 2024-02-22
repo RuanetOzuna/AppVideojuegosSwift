29760e0747dd3758bf01d3c25501de06ad57b54b
Ruanet Alejandro Ozuna Diaz <ruanetozunadiaz@gmail.com>
2024-02-22 10:34:30 -0700

    Initial Commit

---
+++ AppVideojuegos/AppVideojuegosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppVideojuegosApp.swift
+//  AppVideojuegos
+//
+//  Created by Ruanet Ozuna on 22/02/24.
+//
+
+import SwiftUI
+
+@main
+struct AppVideojuegosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppVideojuegos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppVideojuegos
+//
+//  Created by Ruanet Ozuna on 22/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
